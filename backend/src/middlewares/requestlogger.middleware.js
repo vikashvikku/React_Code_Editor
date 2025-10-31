9a61ff04a25adbf3a9fd4bd471faa0499bc52beb
vikashvikku <93948698+vikashvikku@users.noreply.github.com>
2025-10-31 12:21:49 +0530

    Updated request logging messages

--- backend/src/middlewares/requestlogger.middleware.js
+++ backend/src/middlewares/requestlogger.middleware.js
@@ -1,5 +1,4 @@
 
-// This middleware logs the incoming request to the console
 const RequestLoggerMiddleware = (req, res, next) => {
     try {
         const httpMethod = req.method;
@@ -7,12 +6,11 @@ const RequestLoggerMiddleware = (req, res, next) => {
         const url = req.originalUrl || req.url;
         const timeStamp = new Date().toISOString(); 
 
-        console.log(`📌 Request ${httpMethod} ${url} from ${ip} at ${timeStamp}`);
-        // 📌 Request GET /api/users from 192.168.1.10 at 2025-03-11T14:30:00.789Z
+        console.log(` Request ${httpMethod} ${url} from ${ip} at ${timeStamp}`);
 
         next();
     } catch (error) {
-        console.error(`❌ Error while logging the request ${error}`);
+        console.error(` Error while logging the request ${error}`);
         res.status(error.statusCode || 500).json({
             success: false,
             message: error.message || `Internal server error`
